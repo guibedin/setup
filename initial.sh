c4847d13be070183a3e34b1674ccc34c93b4f45f
Guilherme Bedin <guilherme0bedin@gmail.com>
2023-11-27 12:54:02 +0000

    setup new pc

--- initial.sh
+++ initial.sh
@@ -1,9 +1,14 @@
-# setup
-Setup a new PC
+#!/bin/bash
 
 ## Install Git
-```sh
 sudo apt-get install -y git
 git config --global user.email "guilherme0bedin@gmail.com"
 git config --global user.name "Guilherme Bedin"
-```
+
+## Instal ansible
+python3 -m venv .venv
+source .venv/bin/activate
+pip install ansible ansible-vault
+
+## 
+
